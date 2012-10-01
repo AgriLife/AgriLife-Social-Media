c903104d42be7c59406caac36908724369431ca5
J. Aaron Eaton <aaron@channeleaton.com>
2012-10-01 09:22:33 -0500

    Initial commit

---
+++ ag-social-media.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: AgriLife Social Media
+ * Plugin URI: https://github.com/channeleaton/AgriLife-Social-Media
+ * Description: Social media widget for Texas A&M AgriLife
+ * Version: 0.2
+ * Author: J. Aaron Eaton
+ * Author URI: http://channeleaton.com
+ * License: GPL2
+ */
+
+
